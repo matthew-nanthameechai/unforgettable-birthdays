52fe85cf4d9c9ae0a4ce9a759b33d9651e014fe6
Alyssa <alynicbru@gmail.com>
2024-03-29 14:45:42 +1300

    Take out comments

--- client/components/BirthdayForm.tsx
+++ client/components/BirthdayForm.tsx
@@ -78,35 +78,3 @@ function BirthdayForm() {
 }
 
 export default BirthdayForm
-
-// function AddTodo() {
-//   const [newTodo, setNewTodo] = useState('')
-//   const mutation = useAddTodo()
-
-//   const handleChange = (e: React.ChangeEvent) => {
-//     setNewTodo(e.target.value)
-//   }
-
-//   const handleSubmit = (e: React.FormEvent) => {
-//     console.log(newTodo)
-//     e.preventDefault()
-//     mutation.mutate({name: newTodo})
-//     setNewTodo('')
-//   }
-
-//   return (
-//     <>
-//       <div>
-//         <form onSubmit={handleSubmit}>
-//           <input
-//             onChange={handleChange}
-//             value={newTodo}
-//             className="new-todo"
-//             placeholder="What needs to be done?"
-//             autoFocus={true}
-//           />
-//         </form>
-//       </div>
-//     </>
-//   )
-// }
